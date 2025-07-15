64c471bce24567a2bafb7ba65ece2dec42008cc9
Madhan-dharu <madhangnanasekar06@gmail.com>
2025-07-15 03:10:31 -0700

    Add files via upload

---
+++ patten.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main(){
+
+int choice;
+    float balance = 1000.00 , amount;
+    while(1){
+
+            printf("1.balance\n");
+            printf("2.deposite money\n");
+            printf("3.withdraw money\n");
+            printf("4.exit\n");
+
+            printf("enter a choice :");
+            scanf("%d" ,&choice);
+
+            switch(choice){
+            case 1:
+                printf("your balance is : rs%2.f\n" ,balance);
+                break;
+            case 2:
+                printf("enter the deposite amount :");
+                scanf("%f" , &amount);
+                balance+= amount;
+                break;
+            case 3:
+                printf("enter the withdraw amount :");
+                scanf("%f" , &amount);
+                balance-= amount;
+                break;
+            case 4:
+                printf("thank you");
+                return 0;
+
+            default :
+                printf("Invalid statement/n");
+            }
+
